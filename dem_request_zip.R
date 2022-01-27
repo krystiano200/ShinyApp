3d59e30d82d58179b138613157985af48131779c
krystiano200 <48186326+krystiano200@users.noreply.github.com>
2022-01-27 17:38:49 +0100

    Update dem_request_zip.R

--- dem_request_zip.R
+++ dem_request_zip.R
@@ -4,7 +4,8 @@ library(stars)
 library(dplyr)
 library(stringr)
 
-krajkowo = read_sf("krajkowo.shp")
+#setwd("C:/Users/Krystian/Desktop/Aplikacja")
+krajkowo = read_sf("shp/krajkowo.shp")
 
 
 req_df = DEM_request(krajkowo)
@@ -17,15 +18,19 @@ req_df_zip = req_df %>% filter(str_sub(URL, start = -4, end = -1) == ".zip") %>%
 req_df_zip_fil = req_df_zip %>% filter(seriesID == 72586)
 
 
+#TEMP
+my_dir = tempdir()
 
-#pobieramy do swojego folderu
-tile_download(req_df_zip_fil, outdir = "./ZIP")
+tile_download(req_df_zip_fil, outdir = my_dir)
 
 
 sheetnames = req_df_zip_fil$sheetID
 sheetnames
 
-files = dir("ZIP/")
+
+
+
+files = dir(my_dir)
 files
 
 
@@ -33,14 +38,47 @@ pattern = c(paste0(sheetnames, collapse="|"))
 
 index = grep(pattern,files)
 
+
 dem_filenames = files[index]
-dem_filenames
+
+###
+sheetnames_p = c()
+for(i in dem_filenames){
+  if(str_sub(i, start = -5) == 'p.asc'){
+    print(i)
+    sheetnames_p = c(sheetnames_p,i)
+  }
+}
+
+sheetnames_p
+setwd(my_dir)
+
+types = rep("numeric", 3)
+
+read_asc = function(x){
+  types = rep("numeric", 3)
+  read.table(x,colClasses = types , col.names = c("X", "Y", "Z") )
+}
+
+img_dem_zip = lapply(sheetnames_p, read_asc)
 
 
-#przy otwieraniu jednego z plików jest bład.
-img = read_stars("ZIP/N-33-142-B-d-4-2-4_o.asc")
+my_st_as_stars = function(x){
+  st_as_stars(x , coords = c("X", "Y"), crs = "epsg:2180" )
+}
 
 
+img_dem_zip_stars = lapply(img_dem_zip, my_st_as_stars)
+
+plot(img_dem_zip_stars[[1]], col = terrain.colors(99, alpha = NULL))
+
+
+
+
+  
+#tu mamy blad
+
+img_join = do.call(st_mosaic, img_dem_zip_stars)
 
 
 
