bbf11d4f2a64da35ab09459a980e624dd11469ac
krystiano200 <48186326+krystiano200@users.noreply.github.com>
2022-02-08 19:09:32 +0100

    Update rgugik_shinyapp.R

--- rgugik_shinyapp.R
+++ rgugik_shinyapp.R
@@ -13,14 +13,12 @@ library(stringr)
 
 
 
-#pliki = dir("./")
+
 
 morasko = st_point(c(16.942, 52.464))
 morasko = st_sfc(morasko, crs = 4326)
 '%nin%' = Negate('%in%')
-
-
-
+files = dir("./")
 m = mapview(morasko)@map
 testsf = NULL
 
@@ -107,16 +105,15 @@ ui <-   navbarPage(
           tabPanel("Table" ,DT::dataTableOutput("tableortho")), 
           tabPanel(
             "Plot",
-            radioButtons(
-              inputId = "composition",
-              label = "Select composition:",
-              choices = c("CIR", "RGB" , "NDVI")),
-            
-            plotOutput(
-              outputId = "myshapefile"
+              splitLayout(cellWidths = c("50%","50%"),
+                plotOutput(
+                  outputId = "myshapefile"),
+                plotOutput(
+                  outputId = "myshapefile_ndvi"
+                )
             ))
-        )),
-      plotOutput("myshapefile_ndvi"
+        )
+      
                  
                  
       )
@@ -134,21 +131,20 @@ ui <-   navbarPage(
           tabPanel("Table", DT::dataTableOutput("tableorthomap")),
           tabPanel(
             "Plot",
+            splitLayout(cellWidths = c("50%","50%"),
+            plotOutput(
+              outputId = "mapedit_finished"),
+            plotOutput(
+              outputId = "mapedit_ndvi" )
             
-            radioButtons(
-              inputId = "composition2",
-              label = "Select composition:",
-              choices = c("CIR", "RGB", "NDVI"),
-              selected = "CIR")))),
-      plotOutput(
-        outputId = "mapedit_finished"
-        
-      )
-    )
+              
+            )
+            ))
+        ))
   ),
-  
+  navbarMenu("NMT",
   tabPanel(
-    "NMT",
+    "SHAPEFILE",
     sidebarPanel(
       fileInput("filemap2",
                 label = "Wybierz plik shp",    
@@ -165,23 +161,40 @@ ui <-   navbarPage(
         tabPanel("Table", DT::dataTableOutput("tabledem")),
         tabPanel(
           "Plot",
-          radioButtons(
-            inputId = "composition3",
-            label = "Select composition:",
-            choices = c("DTM", "DSM", "TREE"),
-            selected = "DTM"
-          )
-        )  
-        
-      ),
+          
+        )
+      )  
       
-      plotOutput(outputId = "myshapefile2")
-      
-    )
-  )
-)
-
-
+    ),
+    
+    
+    plotOutput(outputId = "myshapefile2")),
+    
+    
+  
+  
+  
+  tabPanel(
+    "Z Mapy",
+    sidebarPanel(
+      tagList(h2("Draw"),
+              editModUI("test-edit"),
+              leafletOutput("edited"))),
+    
+  mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Table", DT::dataTableOutput("tabledemmap")),
+        tabPanel(
+          "Plot"
+  )),
+  plotOutput(
+    outputId = "mapedit_dem")))))
+    
+  
+  
+    
+  
 
 #SERVER
 
@@ -327,23 +340,26 @@ server <- function(input,output){
   })
   
   my_tableortho <-  reactive({
-    ortho_request(map_ortho())
-  })
-  
-  
-  output$tableortho <- DT::renderDataTable({
-    my_tableortho() %>% arrange(desc(year)) %>% 
+    ortho_request(map_ortho()) %>% arrange(desc(year)) %>% 
       select(year,resolution,composition,
              CRS,URL,filename,seriesID)
   })
   
+  
+  output$tableortho <- DT::renderDataTable({
+    my_tableortho() 
+  })
+  my_rows_sel = reactive({input$tableortho_rows_selected})
+  table_sel = reactive({my_tableortho()[my_rows_sel(),]})
+  real_sel = reactive({filter(my_tableortho(), seriesID == table_sel()$seriesID)})
+  
   my_raster <- reactive({
-    my_rows_sel = input$tableortho_rows_selected
-    table_sel = my_tableortho()[my_rows_sel,]
-    real_sel = filter(my_tableortho(), seriesID == table_sel$seriesID)
-    if(paste0(real_sel$filename,".tif") %nin%  pliki){
-    tile_download(real_sel)}
-    filenames = paste0(real_sel$filename,".tif")
+    # my_rows_sel = input$tableortho_rows_selected
+    # table_sel = my_tableortho()[my_rows_sel,]
+    # real_sel = filter(my_tableortho(), seriesID == table_sel$seriesID)
+    if(paste0(real_sel()$filename,".tif") %nin%  files){
+      tile_download(real_sel())}
+    filenames = paste0(real_sel()$filename,".tif")
     if (length(filenames) > 1){
       img = lapply(filenames, read_stars)
       img = do.call(st_mosaic, img)}
@@ -359,65 +375,19 @@ server <- function(input,output){
     
   })
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # 
-  #  my_raster = reactive({
-  #    st_set_crs(map_ortho(),2180)
-  #    req_df = ortho_request(map_ortho())
-  #    if(input$composition == "CIR" | input$composition == "NDVI"){
-  #      req_df = req_df[req_df$composition == "CIR", ]}
-  #    else if(input$composition == "RGB"){
-  #      req_df = req_df[req_df$composition == "RGB", ]
-  #    }
-  #    req_df = req_df[order(-req_df$year), ]
-  #   last_year =  req_df$year[1]
-  #    req_df = req_df[req_df$year == last_year, ]
-  #    tile_download(req_df )#, method = "wget")
-  # 
-  #   filenames = paste0(req_df$filename,".tif")
-  #    if(length(filenames) > 1){
-  #      img = lapply(filenames, read_stars)
-  #      img = do.call(st_mosaic, img)}
-  #    else if(length(filenames) == 1){img = read_stars(filenames)}
-  #    st_crs(img) = 2180
-  #    img = st_crop(img, map_ortho())
-  #    #img
-  # 
-  # 
-  #  })
-  # 
-  # 
   output$myshapefile <- renderPlot({
-    plot(my_raster(), rgb = c(1, 2, 3))})
-    
-  #   #calc_ndvi = function(img) {(img[1] - img[2]) / (img[1] + img[2])}
-  #   #ndvi = st_apply(my_raster(), MARGIN = c("x", "y"), FUN = calc_ndvi)
-  #   if(input$composition == "RGB" | input$composition == "CIR"){
-  #     plot(my_raster(), rgb = c(1, 2, 3), main = input$composition)}
-  #   
-  #   else if(input$composition == "NDVI"){
-  #     calc_ndvi = function(img) {(img[1] - img[2]) / (img[1] + img[2])}
-  #     ndvi = st_apply(my_raster(), MARGIN = c("x", "y"), FUN = calc_ndvi)
-  #     plot(ndvi, main = "NDVI", col = hcl.colors(10, palette = "RdYlGn"))
-  #     
-  #   }
-  #   
-  # })
+    plot(my_raster(), rgb = c(1, 2, 3), main ="")})
+  
+  output$myshapefile_ndvi <- renderPlot({
+    if(real_sel()$composition == "CIR"){
+      calc_ndvi = function(img) {(img[1] - img[2]) / (img[1] + img[2])}
+      ndvi = st_apply(my_raster(), MARGIN = c("x", "y"), FUN = calc_ndvi)
+      plot(ndvi, main = "NDVI", col = hcl.colors(10, palette = "RdYlGn"))}
+    }) 
+  
+  
+  
+ 
   
   map_dem <- reactive({
     req(input$filemap2)
@@ -438,7 +408,7 @@ server <- function(input,output){
                              
     ))
     
-    #st_set_crs(map_dem,2180)
+    
     
     
     
@@ -464,9 +434,7 @@ server <- function(input,output){
   output$tabledem <- DT::renderDataTable({
     my_table() 
   })
-  # my_rows = reactive({
-  #   input$tabledem_rows_selected
-  # })
+ 
   
   
   
@@ -477,47 +445,69 @@ server <- function(input,output){
     table_selected = my_table()[my_rows,]
     real_selected = filter(my_table(), seriesID == table_selected$seriesID)
     
-    #if(paste0(real_selected$filename,".asc") %nin%  pliki){
-    #tile_download(real_selected)}  #, method = "wget")
-    
-    ######
-    
-    
     if(str_sub(real_selected$URL, start = -4, end = -1)[1] == ".zip"){
-      
-      
       print("---------------------------------------------------")
-      tile_download(real_selected, outdir = "./zipdir")
-      files = dir("zipdir/")
-      
+      my_dir = tempdir()
+      tile_download(real_selected, outdir = my_dir)
+      files_zip = dir(my_dir)
       sheetnames = real_selected$sheetID
       
       pattern = c(paste0(sheetnames, collapse="|")) 
-      index = grep(pattern, files)
-      dem_filenames = files[index]
+      index = grep(pattern, files_zip)
+      dem_filenames = files_zip[index]
+      sheetnames_p = c()
+      for(i in dem_filenames){
+        if(str_sub(i, start = -5) == 'p.asc'){
+          sheetnames_p = c(sheetnames_p,i)
+        }
+      }
+      setwd(my_dir)
+      
+      read_asc = function(x){
+        types = rep("numeric", 3)
+        read.table(x, colClasses = types ,col.names = c("X", "Y", "Z") )
+      }
+      
+      my_rast = function(x){
+        y = terra::rast(x, crs = "EPSG:2180")
+        y
+      }
+      
+      # img_dem_zip = lapply(sheetnames_p, read_asc)
+      # img_dem_zip_rast = lapply(img_dem_zip, my_rast)
+      # terra::plot(img_dem_zip_rast[[1]], col = terrain.colors(99, alpha = NULL))
+      # img_dem_zip_rast2 = do.call(terra::mosaic, img_dem_zip_rast)
+      # terra::plot(img_dem_zip_rast2)
+      
       
       
       if (length(dem_filenames) > 1){
-        setwd("zipdir/")
-        img_dem = lapply(dem_filenames, read_stars)
-        img_dem = do.call(st_mosaic, img_dem)}
-      else if(length(dem_filenames) == 1){
-        setwd("zipdir/")
-        img_dem = read_stars(dem_filenames)
+        
+        img_dem = lapply(sheetnames_p, read_asc)
+        img_dem = lapply(img_dem, my_rast)
+        #img_dem = do.call(terra::mosaic, img_dem)
+        img_dem = img_dem[[1]]
         }
-      dem_temp = map_dem()
-      st_crs(img_dem) = 2180
-      if(st_crs(dem_temp) != st_crs(img_dem)){
-        dem_temp = st_transform(dem_temp, st_crs(img_dem))
+      else if(length(dem_filenames) == 1){
+        
+        img_dem = read_asc(dem_filenames)
+        img_dem = my_rast(img_dem)
       }
-      img_dem = st_crop(img_dem , dem_temp)
       
+      dem_temp = map_dem()
+      terra::crs(img_dem) = "EPSG:2180"
+      crs_dem = terra::crs(img_dem)
+      if(st_crs(dem_temp) != crs_dem){
+        dem_temp = st_transform(dem_temp, crs_dem)
+      }
       
-      
-    }
-    else{
-      if(paste0(real_selected$filename,".asc") %nin%  pliki){
-      tile_download(real_selected)}
+      img_dem = terra::crop(img_dem,dem_temp)
+      }
+   
+    
+     else{
+      if(paste0(real_selected$filename,".asc") %nin%  files){
+        tile_download(real_selected)}
       dem_filenames = paste0(real_selected$filename,".asc")}
     
     if (length(dem_filenames) > 1){
@@ -553,62 +543,65 @@ server <- function(input,output){
     req(crud()$finished)
   })
   
-  # my_tableorthomap <- reactive({
-  #   ortho_request(my_polygon())
-  # })
-  # 
-  # output$tableorthomap <- DT::renderDataTable({
-  #   my_tableorthomap() 
-  # })
+  my_tableorthomap <- reactive({
+    ortho_request(my_polygon())
+  })
+
+  output$tableorthomap <- DT::renderDataTable({
+    my_tableorthomap() %>% arrange(desc(year)) %>% 
+      select(year,resolution,composition,
+             CRS,URL,filename,seriesID)
   
-  
-  
+    })
+  my_select = reactive({input$tableorthomap_rows_selected})
+  table_select = reactive({my_tableorthomap()[my_select(),]})
+  real_select = reactive({filter(my_tableorthomap(), seriesID == table_select()$seriesID)})
   
   
   
   my_raster_map <- reactive({
-    req_df_map = ortho_request(my_polygon())
-    if(input$composition2 == "CIR" | input$composition2 == "NDVI"){
-      req_df_map = req_df_map[req_df_map$composition == "CIR", ]}
-    else if(input$composition2 == "RGB"){
-      req_df_map = req_df_map[req_df_map$composition == "RGB", ]}
-    req_df_map = req_df_map[order(-req_df_map$year), ]
-    last_year_map =  req_df_map$year[1]
-    req_df_map = req_df_map[req_df_map$year == last_year_map, ]
-    tile_download(req_df_map )#, method = 'wget')
-    filenames3 = paste0(req_df_map$filename,".tif")
+    #my_select = input$tableorthomap_rows_selected
+    #table_select = my_tableorthomap()[my_select,]
+    #real_select = filter(my_tableorthomap(), seriesID == table_select$seriesID)
     
-    if (length(filenames3) > 1){
-      img_map = lapply(filenames3, read_stars)
+    if(paste0(real_select()$filename,".tif") %nin%  files){
+      tile_download(real_select())
+      }
+    filenames_map = paste0(real_select()$filename,".tif")
+    
+    if (length(filenames_map) > 1){
+      img_map = lapply(filenames_map, read_stars)
       img_map = do.call(st_mosaic, img_map)
-    }else if(length(filenames3) == 1){
-      img_map = read_stars(filenames3)
+      }
+    else if(length(filenames_map) == 1){
+      img_map = read_stars(filenames_map)
+      }
+    orthomap_temp = my_polygon()
+    #st_crs(img_map) = 2180
+    if(st_crs(orthomap_temp) != st_crs(img_map)){
+      orthomap_temp = st_transform(orthomap_temp, st_crs(img_map))
     }
-    
-    temp_poly = my_polygon()
-    if(st_crs(temp_poly) != st_crs(img_map)){
-      temp_poly = st_transform(temp_poly, st_crs(img_map))
-    }
-    img_map = st_crop(img_map, temp_poly)
+    img_map = st_crop(img_map,orthomap_temp)
     img_map
-  })
+    
+    })
   
   output$mapedit_finished <- renderPlot({
+    plot(my_raster_map(), rgb = c(1, 2, 3), main = "aaa")
+})
+
+  output$mapedit_ndvi <- renderPlot({
+    if(real_select()$composition == "CIR"){
+    calc_ndvi = function(img) {(img[1] - img[2]) / (img[1] + img[2])}
+    ndvi_map = st_apply(my_raster_map(), MARGIN = c("x", "y"), FUN = calc_ndvi)
+    plot(ndvi_map, main = "NDVI", col = hcl.colors(10, palette = "RdYlGn"))}
     
-    if(input$composition2 == "RGB" | input$composition2 == "CIR"){
-      plot(my_raster_map(), rgb = c(1, 2, 3), main = input$composition2)}
-    
-    else if(input$composition2 == "NDVI"){
-      calc_ndvi = function(img) {(img[1] - img[2]) / (img[1] + img[2])}
-      ndvi_map = st_apply(my_raster_map(), MARGIN = c("x", "y"), FUN = calc_ndvi)
-      plot(ndvi_map, main = "NDVI", col = hcl.colors(10, palette = "RdYlGn"))
-      
-    }
-    
-    
-    
-    
-  })}  
+  
+  }) 
+
+}
+  
+  
 
 shinyApp(server = server, ui = ui)
 
