4b143facf771b9862f183d6e0d106de160abcbcb
krystiano200 <48186326+krystiano200@users.noreply.github.com>
2022-02-15 13:01:44 +0100

    Update rgugik_shinyapp.R

--- rgugik_shinyapp.R
+++ rgugik_shinyapp.R
@@ -1,5 +1,3 @@
-#SHINY APP SIERPIEN
-
 library(shiny)
 library(shinythemes)
 library(rgugik)
@@ -11,10 +9,6 @@ library(leaflet)
 library(dplyr)
 library(stringr)
 
-
-
-
-
 morasko = st_point(c(16.942, 52.464))
 morasko = st_sfc(morasko, crs = 4326)
 '%nin%' = Negate('%in%')
@@ -22,7 +16,6 @@ files = dir("./")
 m = mapview(morasko)@map
 n = mapview(morasko)@map
 testsf = NULL
-
 
 
 #USER INTERFACE
@@ -46,8 +39,8 @@ ui <-   navbarPage(
                  outputId = "distplot",
                  width = "100%"  , 
                  height = "700px"
-               )
-             )
+        )
+      )
     ),
     
     tabPanel(
@@ -112,14 +105,12 @@ ui <-   navbarPage(
                           outputId = "myshapefile"),
                         plotOutput(
                           outputId = "myshapefile_ndvi"
-                        )
-            ))
+            )
+          )
         )
-        
-        
-        
       )
-    ),
+    )
+  ),
     
     tabPanel(
       "Z MAPY",
@@ -141,8 +132,10 @@ ui <-   navbarPage(
                         
                         
             )
-          ))
-      ))
+          )
+        )
+      )
+    )
   ),
   navbarMenu("NMT",
              tabPanel(
@@ -180,14 +173,10 @@ ui <-   navbarPage(
                  plotOutput(
                    outputId = "mapedit_dem")
              
-             )))
+      )
+    )
+  )
   
-
-
-
-
-
-
 
 #SERVER
 
@@ -347,9 +336,7 @@ server <- function(input,output){
   real_sel = reactive({filter(my_tableortho(), seriesID == table_sel()$seriesID)})
   
   my_raster <- reactive({
-    # my_rows_sel = input$tableortho_rows_selected
-    # table_sel = my_tableortho()[my_rows_sel,]
-    # real_sel = filter(my_tableortho(), seriesID == table_sel$seriesID)
+   
     if(paste0(real_sel()$filename,".tif") %nin%  files){
       tile_download(real_sel())}
     filenames = paste0(real_sel()$filename,".tif")
@@ -400,108 +387,35 @@ server <- function(input,output){
                              
                              
     ))
-    
-    
-    
-    
-    
-  })
-  
-  
-  
-  
+    })
   
   my_table <-  reactive({
     DEM_request(map_dem()) %>%
       select(year,format,resolution,
              CRS,filename,product,seriesID, URL) %>%
       filter(product %in% c("DTM", "DSM")) %>% 
+      filter(str_sub(URL, start = -4, end = -1) == ".asc") %>% 
       arrange(desc(year))
     
   })
   
-  
-  
-  
-  
   output$tabledem <- DT::renderDataTable({
     my_table() 
   })
-  
-  
-  
   
   nmt_img <- reactive({
     
     files = dir("./")
     my_rows = input$tabledem_rows_selected
     table_selected = my_table()[my_rows,]
+   
     real_selected = filter(my_table(), seriesID == table_selected$seriesID)
     
-    if(str_sub(real_selected$URL, start = -4, end = -1)[1] == ".zip"){
-      print("---------------------------------------------------")
-      my_dir = tempdir()
-      tile_download(real_selected, outdir = my_dir)
-      files_zip = dir(my_dir)
-      sheetnames = real_selected$sheetID
-      
-      pattern = c(paste0(sheetnames, collapse="|")) 
-      index = grep(pattern, files_zip)
-      dem_filenames = files_zip[index]
-      sheetnames_p = c()
-      for(i in dem_filenames){
-        if(str_sub(i, start = -5) == 'p.asc'){
-          sheetnames_p = c(sheetnames_p,i)
-        }
-      }
-      setwd(my_dir)
-      
-      read_asc = function(x){
-        types = rep("numeric", 3)
-        read.table(x, colClasses = types ,col.names = c("X", "Y", "Z") )
-      }
-      
-      my_rast = function(x){
-        y = terra::rast(x, crs = "EPSG:2180")
-        y
-      }
-      
-      # img_dem_zip = lapply(sheetnames_p, read_asc)
-      # img_dem_zip_rast = lapply(img_dem_zip, my_rast)
-      # terra::plot(img_dem_zip_rast[[1]], col = terrain.colors(99, alpha = NULL))
-      # img_dem_zip_rast2 = do.call(terra::mosaic, img_dem_zip_rast)
-      # terra::plot(img_dem_zip_rast2)
-      
-      
-      
-      if (length(dem_filenames) > 1){
-        
-        img_dem = lapply(sheetnames_p, read_asc)
-        img_dem = lapply(img_dem, my_rast)
-        #img_dem = do.call(terra::mosaic, img_dem)
-        img_dem = img_dem[[1]]
-      }
-      else if(length(dem_filenames) == 1){
-        
-        img_dem = read_asc(dem_filenames)
-        img_dem = my_rast(img_dem)
-      }
-      
-      dem_temp = map_dem()
-      terra::crs(img_dem) = "EPSG:2180"
-      crs_dem = terra::crs(img_dem)
-      if(st_crs(dem_temp) != crs_dem){
-        dem_temp = st_transform(dem_temp, crs_dem)
-      }
-      
-      img_dem = terra::crop(img_dem,dem_temp)
-    }
+    if(paste0(real_selected$filename,".asc") %nin%  files){
+      tile_download(real_selected)}
     
     
-    else{
-      if(paste0(real_selected$filename,".asc") %nin%  files){
-        tile_download(real_selected)}
-      dem_filenames = paste0(real_selected$filename,".asc")}
+    dem_filenames = paste0(real_selected$filename,".asc")
     
     if (length(dem_filenames) > 1){
       img_dem = lapply(dem_filenames, read_stars)
@@ -513,11 +427,8 @@ server <- function(input,output){
       dem_temp = st_transform(dem_temp, st_crs(img_dem))
     }
     img_dem = st_crop(img_dem , dem_temp)
-    
+  
   })
-  
-  
-  
   
   
   output$myshapefile2 <- renderPlot({
@@ -527,25 +438,22 @@ server <- function(input,output){
   
   
   
-  
-  
-  
-  
   crud <- callModule(editMod, "test-edit", m, "breweries")
   my_polygon <- reactive({
     req(crud()$finished)
   })
   
   my_tableorthomap <- reactive({
-    ortho_request(my_polygon())
+    ortho_request(my_polygon())%>% 
+      select(year,resolution,composition,
+             CRS,URL,filename,seriesID) %>% 
+      arrange(desc(year))
   })
   
   output$tableorthomap <- DT::renderDataTable({
-    my_tableorthomap() %>% arrange(desc(year)) %>% 
-      select(year,resolution,composition,
-             CRS,URL,filename,seriesID)
-    
+    my_tableorthomap()  
   })
+  
   my_select = reactive({input$tableorthomap_rows_selected})
   table_select = reactive({my_tableorthomap()[my_select(),]})
   real_select = reactive({filter(my_tableorthomap(), seriesID == table_select()$seriesID)})
@@ -590,67 +498,65 @@ server <- function(input,output){
     
   }) 
   
+
+#
+ crud2 <- callModule(editMod, "test-edit2", m, "breweries")
+ 
+ my_polygon2 <- reactive({
+   req(crud2()$finished)
+ })
+
+ my_tabledem_map <- reactive({
+   DEM_request(my_polygon2()) %>%
+     select(year,format,resolution,
+            CRS,filename,product,seriesID, URL) %>%
+     filter(product %in% c("DTM", "DSM")) %>%
+     filter(str_sub(URL, start = -4, end = -1) == ".asc") %>% 
+     arrange(desc(year))
+ })
+
+ output$tabledemmap <- DT::renderDataTable({
+   my_tabledem_map()
+ })
+
+ my_select_map = reactive({input$tabledemmap_rows_selected})
+ table_select_map = reactive({my_tabledem_map()[my_select_map(),] })
+ real_select_map = reactive({filter(my_tabledem_map(), seriesID == table_select_map()$seriesID)})
+
+
+
+ my_dem_map <- reactive({
+
+
+  
+   tile_download(real_select_map())
+   
+   dem_filenames_map = paste0(real_select_map()$filename,".asc")
+
+   if (length(dem_filenames_map) > 1){
+     dem_map = lapply(dem_filenames_map, read_stars)
+     dem_map = do.call(st_mosaic, dem_map)
+   }
+   else if(length(dem_filenames_map) == 1){
+     dem_map = read_stars(dem_filenames_map)
+   }
+   temp_poly_dem = my_polygon2()
+   st_crs(dem_map) = 2180
+   if(st_crs(temp_poly_dem) != st_crs(dem_map)){
+     temp_poly_dem = st_transform(temp_poly_dem, st_crs(dem_map))
+   }
+   dem_map = st_crop(dem_map,temp_poly_dem)
+   dem_map
+
+ })
+
+ output$mapedit_dem <- renderPlot({
+   plot(my_dem_map(),col = terrain.colors(99, alpha = NULL), main = "NMT")
+ })
+
+
+
 }
-# 
-crud2 <- callModule(editMod, "test-edit2", m, "breweries")
-my_polygon2 <- reactive({
-  req(crud2()$finished)
-})
-
-my_tabledem_map <- reactive({
-  DEM_request(my_polygon2()) %>%
-    select(year,format,resolution,
-           CRS,filename,product,seriesID, URL) %>%
-    filter(product %in% c("DTM", "DSM")) %>%
-    arrange(desc(year))
-})
-
-output$tabledemmap <- DT::renderDataTable({
-  my_tabledem_map()
-})
-
-my_select_map = reactive({input$tabledemmap_rows_selected})
-table_select_map = reactive({my_tabledemmap()[my_select_map(),] })
-real_select_map = reactive({filter(my_tabledem_map(), seriesID == table_select_map()$seriesID)})
-
-
-
-my_dem_map <- reactive({
-
-
-  if(paste0(real_select_map()$filename,".asc") %nin%  files){
-    tile_download(real_select_map())
-  }
-  dem_filenames_map = paste0(real_select_map()$filename,".asc")
-
-  if (length(dem_filenames_map) > 1){
-    dem_map = lapply(dem_filenames_map, read_stars)
-    dem_map = do.call(st_mosaic, dem_map)
-  }
-  else if(length(dem_filenames_map) == 1){
-    dem_map = read_stars(dem_filenames_map)
-  }
-  temp_poly_dem = my_polygon2()
-  #st_crs(img_map) = 2180
-  if(st_crs(temp_poly_dem) != st_crs(dem_map)){
-    temp_poly_dem = st_transform(temp_poly_dem, st_crs(dem_map))
-  }
-  dem_map = st_crop(dem_map,temp_poly_dem)
-  dem_map
-
-})
-
-output$mapedit_dem <- renderPlot({
-  plot(my_dem_map(),col = terrain.colors(99, alpha = NULL), main = "NMT")
-})
-
-
-
-
-
-
-
-
 
 shinyApp(server = server, ui = ui)
 
